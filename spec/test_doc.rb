cbbec62e6c474ddae3e1da34fbe5199af265f5ee
Bernard Lambeau <blambeau@gmail.com>
2013-10-17 17:13:42 +0200

    Document aggregators, operators and predicates.

--- spec/test_doc.rb
+++ spec/test_doc.rb
@@ -6,5 +6,18 @@ module Alf
       Doc.const_defined?(:VERSION).should be_true
     end
 
+    Path.backfind('doc').glob('**/*.yml') do |file|
+      describe file.basename do
+
+        subject{ file.load }
+
+        it 'is valid YAML' do
+          lambda{
+            subject
+          }.should_not raise_error
+        end
+      end
+    end
+
   end
 end
